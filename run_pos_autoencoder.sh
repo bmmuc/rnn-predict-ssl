b2ddc7158c2e5814918489ec17f487e82ee30e88
bmmuc <bmmuc@cin.ufpe.br>
2023-02-16 20:39:50 -0300

    add pos autoencoder

---
+++ run_pos_autoencoder.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#SBATCH -o slurm-%j.out # Write the log here
+#SBATCH --gres=gpu:1 # Ask for 1 GPU
+#SBATCH --output=job_output.txt
+#SBATCH --error=job_error.txt
+conda activate bmmuc
+git pull
+python train_pos_autoencoder
