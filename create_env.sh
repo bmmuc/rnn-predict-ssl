cb2b0cd7748d40e160779bb227197cc09c23f0cb
bmmuc <bmmuc@cin.ufpe.br>
2023-02-16 20:18:15 -0300

    fix create_env

--- create_env.sh
+++ create_env.sh
@@ -1,6 +1,5 @@
 conda create -n bmmuc python=3.8.0
 conda activate bmmuc
-cd rnn-predict-ssl
 git pull
 pip install -r requirements.txt
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
