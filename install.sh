fd81e12057e84bf320697d0f58e655f3b2d6c64f
bmmuc <bmmuc@cin.ufpe.br>
2023-02-18 19:06:01 -0300

    update install and run pos

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@
 #SBATCH --cpus-per-task=10
 . "/usr/local/anaconda3/etc/profile.d/conda.sh"
 conda activate bmmuc
-pip install -r env.txt
+pip install -r requirements.txt
 pip install torch --upgrade --force-reinstall --extra-index-url https://download.pytorch.org/whl/cu116
