a0cd2db2d80aeb8002f15fe98f3eed8732515655
bmmuc <bmmuc@cin.ufpe.br>
2023-02-17 22:32:04 -0300

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#SBATCH --job-name=install_bmmuc
+#SBATCH --ntasks=1
+#SBATCH --gpus=0
+#SBATCH --cpus-per-task=10
+. "/usr/local/anaconda3/etc/profile.d/conda.sh"
+conda activate bmmuc
+pip install -r env.txt
+pip install torch --upgrade --force-reinstall --extra-index-url https://download.pytorch.org/whl/cu116
