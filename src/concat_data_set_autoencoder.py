9b9c5dab98fd64e11f7ad99b1d67aa7dfb6cfe80
bmmuc <bmmuc@cin.ufpe.br>
2023-03-01 19:47:36 -0300

    updated to get more trian

--- src/concat_data_set_autoencoder.py
+++ src/concat_data_set_autoencoder.py
@@ -7,15 +7,18 @@ import tqdm
 from src.aux_idx import Aux
 import ipdb
 # from create_window import create_window
-def create_window(data, window_size, horizon = 1):
-        out = []
-        L = len(data)
 
-        for i in range(L - window_size - horizon):
-            window = data[i : i+window_size, :]
-            label = data[i+window_size : i+window_size+horizon, :]
-            out.append((window,label))
-        return out
+
+def create_window(data, window_size, horizon=1):
+    out = []
+    L = len(data)
+
+    for i in range(L - window_size - horizon):
+        window = data[i: i+window_size, :]
+        label = data[i+window_size: i+window_size+horizon, :]
+        out.append((window, label))
+    return out
+
 
 class ConcatDataSetAutoencoder(Dataset):
     """Multi-variate Time-Series ConcatDataset for *.txt file
@@ -26,22 +29,24 @@ class ConcatDataSetAutoencoder(Dataset):
     Returns:
         [sample, next_sample]
     """
-    def __init__(self, 
-                root_dir =  '', 
-                num_of_data_sets = 50000,
-                window = 5,
-                type_of_data = 'train',
-                horizon = 1,
-                is_pos = True,
-                should_test_overffit = False,
-                should_test_the_new_data_set = True,
-                is_to_debug = False):
+
+    def __init__(self,
+                 root_dir='',
+                 num_of_data_sets=50000,
+                 window=5,
+                 type_of_data='train',
+                 horizon=1,
+                 is_pos=True,
+                 should_test_overffit=False,
+                 should_test_the_new_data_set=True,
+                 is_to_debug=False):
 
         self.root_dir = root_dir
         self.num_of_data_sets = num_of_data_sets
         self.window = window
         self.is_pos = is_pos
-        self.indexes_pos = [0, 1, 2, 3, 4, 5, 6, 7, 11, 12, 13, 14, 18, 19, 20, 21, 25, 26, 30, 31, 35, 36]
+        self.indexes_pos = [0, 1, 2, 3, 4, 5, 6, 7, 11, 12,
+                            13, 14, 18, 19, 20, 21, 25, 26, 30, 31, 35, 36]
         aux_index = Aux.is_vel
         self.bool_indexes = []
 
@@ -57,7 +62,7 @@ class ConcatDataSetAutoencoder(Dataset):
         self.should_test_the_new_data_set = should_test_the_new_data_set
         self.is_to_debug = is_to_debug
         self.data_windows = list()
-        self.data_labels = list()   
+        self.data_labels = list()
         self.type = type_of_data
 
         print(root_dir)
@@ -67,7 +72,7 @@ class ConcatDataSetAutoencoder(Dataset):
     def _shuffle(self, X, y):
         joined_lists = list(zip(X, y))
         random.shuffle(joined_lists)
-        
+
         X, y = zip(*joined_lists)
 
         return X, y
@@ -77,14 +82,15 @@ class ConcatDataSetAutoencoder(Dataset):
         # if self.is_pos:
         #     self.data_windows = np.load(f'./datasets/{self.type}_data_windows.npy', allow_pickle=True)
         # else:
-        self.data_windows = np.load(f'./datasets_full/{self.type}_data_windows_full.npy', allow_pickle=True)
+        self.data_windows = np.load(
+            f'./datasets_full/{self.type}_data_windows_full_90.npy', allow_pickle=True)
         # ipdb.set_trace()
         print(f'Loaded the {self.type} data set')
 
     def __getitem__(self, idx):
         # ipdb.set_trace()
         # ipdb.set_trace()
-        if(self.is_pos):
+        if (self.is_pos):
             # X = self.data_windows[idx, :, self.bool_indexes]
             # X = X.reshape(-1, X.shape[0]).astype(np.float32)
 
@@ -108,7 +114,7 @@ class ConcatDataSetAutoencoder(Dataset):
         # return 0
         # print(X.shape, y.shape)
         X = X.reshape(-1, X.shape[0]).astype(np.float32)
-        return [X, X]    
+        return [X, X]
 
     def __len__(self):
         return len(self.data_windows)
@@ -116,9 +122,9 @@ class ConcatDataSetAutoencoder(Dataset):
 
 if __name__ == '__main__':
 
-    test = ConcatDataSetAutoencoder(root_dir= '/home/bmmuc/Documents/robocin/rnn/all_data/data-v4', num_of_data_sets= 100, horizon=1, is_to_debug=True,
-        is_pos=False)
-    test = ConcatDataSetAutoencoder(root_dir= '/home/bmmuc/Documents/robocin/rnn/all_data/data-v4', num_of_data_sets= 100, horizon=1, is_to_debug=True,
-        is_pos=True)
+    test = ConcatDataSetAutoencoder(root_dir='/home/bmmuc/Documents/robocin/rnn/all_data/data-v4', num_of_data_sets=100, horizon=1, is_to_debug=True,
+                                    is_pos=False)
+    test = ConcatDataSetAutoencoder(root_dir='/home/bmmuc/Documents/robocin/rnn/all_data/data-v4', num_of_data_sets=100, horizon=1, is_to_debug=True,
+                                    is_pos=True)
     test.__getitem__(0)
     # test.__getitem__(40602)lf.root_dir + f'/positions-{i}.txt'), dtype=np.float32)
