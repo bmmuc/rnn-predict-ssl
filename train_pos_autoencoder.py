669e53ef238e7fda828d56b5f2e88b7e4c208a69
bmmuc <bmmuc@cin.ufpe.br>
2023-03-04 13:29:56 -0300

    test_aloprado_2

--- train_pos_autoencoder.py
+++ train_pos_autoencoder.py
@@ -10,13 +10,13 @@ import pytz
 from datetime import datetime, timezone
 import os
 
-BATCH_SIZE = 128
-HIDDEN_SIZE = 1024
+BATCH_SIZE = 1024
+HIDDEN_SIZE = 512
 WINDOW_SIZE = 50
 HORIZON_SIZE = 1
 INPUT_SIZE = 38
 EPOCHS = 100
-LR = 1e-3
+LR = 1e-5
 
 NUM_WORKERS = 20
 
